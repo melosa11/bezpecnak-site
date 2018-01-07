fa18e2f1d7e7719b89e33db9536ca1d5a4829b55
melosa18 <david@sutor.sk>
2018-01-07 17:25:58 +0100

    Big Change

--- responsive.js
+++ responsive.js
@@ -1,5 +1,15 @@
+// Open and close the sidebar on medium and small screens
+function w3_open() {
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
+}
+function w3_close() {
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
+}
+
 // Change style of top container on scroll
-window.onscroll = function () { myFunction() };
+window.onscroll = function() {myFunction()};
 function myFunction() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
         document.getElementById("myTop").classList.add("w3-card-4", "w3-animate-opacity");
